e0d3a4abe4d43bce1699f942727c207743c6ab42
Eldrick-beaumont <eldrick.beaumont@mail.mcgill.ca>
2026-02-25 13:37:07 -0500

    Modifier le log_parser.py pour que le report montrer les bonnes metrics et Ajuster l'interface pour display le bon noeud ciblé

--- tools/log_parser.py
+++ tools/log_parser.py
@@ -10,18 +10,27 @@ def parseLogs():
     with open(LOG_PATH, "r") as f:
         logs = f.readlines()
     
+    requests = []
     
     for log in logs:
         curEntry = json.loads(log)
         if curEntry["route"] == "kill":
             #start new downtime entry
-            data.append({"server": curEntry["sent_to"], "down_since": datetime.strptime(curEntry["timestamp"], "%Y-%m-%d %H:%M:%S"), "service_back_up": None, "requests_during_downtime": 0, "failed_requests_during_downtime": 0})
+            data.append({"server": curEntry["sent_to"], "down_since": datetime.strptime(curEntry["timestamp"], "%Y-%m-%d %H:%M:%S"), "service_back_up": None, "requests_around_downtime": 0, "failed_requests_during_downtime": 0})
         elif curEntry["route"] == "activateSpare":
             data[-1]["service_back_up"] = datetime.strptime(curEntry["timestamp"], "%Y-%m-%d %H:%M:%S")
-        elif data and data[-1]["service_back_up"] == None and data[-1]["server"] == curEntry["sent_to"]:
-            data[-1]["requests_during_downtime"] += 1
-            if curEntry["code"] != 200:
-                data[-1]["failed_requests_during_downtime"] += 1
+        else:
+           requests.append((datetime.strptime(curEntry["timestamp"], "%Y-%m-%d %H:%M:%S"), curEntry["code"], curEntry["sent_to"]))
+    
+    for curEntry in requests:
+        req_ts = curEntry[0].timestamp()
+        for downtime in data:
+            start_ts = downtime["down_since"].timestamp() - 5
+            end_ts = downtime["service_back_up"].timestamp() + 5  
+            if downtime["server"] == curEntry[2] and start_ts <= req_ts and req_ts <= end_ts:
+                downtime["requests_around_downtime"] += 1
+                if curEntry[1] != 200:
+                    downtime["failed_requests_during_downtime"] += 1
     
     
     numDowntimes = len(data)
@@ -30,11 +39,13 @@ def parseLogs():
         for downtime in data:
             f.write(f"--- Panne {data.index(downtime) + 1} ---\n")
             f.write(f"Serveur: {downtime['server']}\n")
-            f.write(f"En panne depuis: {downtime['down_since']}\n")
+            f.write(f"Debut de la panne: {downtime['down_since']}\n")
             f.write(f"Service retabli: {downtime['service_back_up']}\n")
-            f.write(f"Duree de la panne: {downtime['service_back_up'] - downtime['down_since']}\n")
-            f.write(f"Requetes pendant la panne: {downtime['requests_during_downtime']}\n")
+            f.write(f"Duree de la panne (Temps de bascule): {downtime['service_back_up'] - downtime['down_since']}\n")
+            f.write(f"Nombre de requetes autour de la panne (Fenetre de 5s avant et apres): {downtime['requests_around_downtime']}\n")
             f.write(f"Requetes echouees pendant la panne: {downtime['failed_requests_during_downtime']}\n")
+            error_rate = int((downtime['failed_requests_during_downtime'] / downtime['requests_around_downtime']) * 100)
+            f.write(f"Taux d'erreurs pendant la bascule: {downtime['failed_requests_during_downtime']}/{downtime['requests_around_downtime']} = ~{error_rate}%\n")
             f.write("\n")
             
     
